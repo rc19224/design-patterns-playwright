99957c1f7b5d26809a342a3f838256a80e8232ab
Racharla Vishwas <racharla.vishwas@ryan.com>
2025-09-10 11:20:56 +0530

    test with built-in fixtures

--- tests/fixtures.spec.ts
+++ tests/fixtures.spec.ts
@@ -1,12 +1,8 @@
-import {test, expect, chromium} from "@playwright/test";
+import {test, expect} from "@playwright/test";
 
-test("Sign in Button is visible", async() => {
-    const browser = await chromium.launch();
-    const page = await browser.newPage();
-
+// test with built-in fixtures
+test("Sign in Button is visible", async({page}) => {
     await page.goto("https://binaryville.com/account")
     const signInButton = page.getByRole("button", {name : "Sign in"})
     await expect(signInButton).toBeVisible()
-
-    await browser.close();
 })
